376363926bf061a7baaec46147c8cf3b25390828
antkss <88892713+antkss@users.noreply.github.com>
2024-03-28 15:55:02 +0700

    Update install-gui.sh

--- install-gui.sh
+++ install-gui.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt install xvfb xfce4 xfce4-goodies mpv kdenlive simplescreenrecorder firefox-esr plank papirus-icon-theme dbus-x11 neofetch krita --assume-yes
+sudo apt install xvfb xfce4 mpv firefox-esr dbus-x11 neofetch --assume-yes
 # Download Chrome Remote Desktop, install it, then delete downloaded .deb package
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg -i chrome-remote-desktop*
