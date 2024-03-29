3f156cd583f22e57df4eaf785e50399d6041d0c4
antkss <88892713+antkss@users.noreply.github.com>
2024-03-29 20:18:02 +0700

    Update root.sh

--- root.sh
+++ root.sh
@@ -4,5 +4,4 @@ echo "lmao ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo "deb http://http.kali.org/kali kali-rolling main contrib non-free non-free-firmware" >> /etc/apt/sources.list
 wget "https://http.kali.org/pool/main/k/kali-archive-keyring/kali-archive-keyring_2024.1_all.deb"
 dpkg -i kali-archive-keyring_2024.1_all.deb
-apt update && apt upgrade -y && apt install xfce4 firefox
 yes lmao | login
